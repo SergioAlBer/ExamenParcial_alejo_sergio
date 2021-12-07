dff291fa923749af399348975a1a696092b81968
SERGIO <SERGIO@DESKTOP-F2MRB7S>
2021-12-06 21:15:56 -0500

    crearyestructurar_paquetes_y_clases

---
+++ Parcial_AlejoSergio/src/main/java/Usuario/Cliente.java
@@ -0,0 +1,41 @@
+
+package Usuario;
+
+
+public class Cliente {
+    
+    private String DNI;
+    private String nombre;
+    private String direccion;
+    
+    
+    public boolean depositar(Cuenta c,float monto){
+        boolean result=false;
+        
+        
+        return result;
+        
+    }
+    
+    public boolean retirar(Cuenta c,float monto){
+        boolean result=false;
+        
+        
+        return result;
+        
+    }
+    
+    public boolean transferir(Cuenta c,float monto){
+        boolean result=false;
+        
+        
+        return result;
+        
+    }
+    
+    public Operacion[] consultarMovimiento(Cuenta c){
+        
+    }
+    
+    
+}
---
+++ Parcial_AlejoSergio/src/main/java/Usuario/Cuenta.java
@@ -0,0 +1,19 @@
+
+package Usuario;
+
+import java.util.Date;
+
+public class Cuenta {
+    
+    private String numero;
+    private Date fechaApertura;
+    private float saldo;
+    
+    
+    
+    
+    
+    
+    
+    
+}
